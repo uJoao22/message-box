13b6fd6ab7643c41199921e29da73cb55b9b388e
uJoao22 <88169092+uJoao22@users.noreply.github.com>
2021-09-17 13:51:12 -0300

    add program

---
+++ MainForm.Designer.cs
@@ -0,0 +1,120 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: uJão
+ * Date: 29/06/2021
+ * Time: 11:03
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace PjCxMessage
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.button4 = new System.Windows.Forms.Button();
+			this.button5 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(200, 50);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(400, 40);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "Caixa de mensagem apenas com texto";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(200, 115);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(400, 40);
+			this.button2.TabIndex = 1;
+			this.button2.Text = "Caixa de mensagem com texto e titulo";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.Button2Click);
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(200, 180);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(400, 40);
+			this.button3.TabIndex = 2;
+			this.button3.Text = "Caixa de mensagem com texto, titulo e botões";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.Button3Click);
+			// 
+			// button4
+			// 
+			this.button4.Location = new System.Drawing.Point(200, 245);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(400, 40);
+			this.button4.TabIndex = 3;
+			this.button4.Text = "Caixa de mensagem com texto, titulo, botões e icones";
+			this.button4.UseVisualStyleBackColor = true;
+			this.button4.Click += new System.EventHandler(this.Button4Click);
+			// 
+			// button5
+			// 
+			this.button5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+			this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button5.ForeColor = System.Drawing.Color.White;
+			this.button5.Location = new System.Drawing.Point(695, 311);
+			this.button5.Name = "button5";
+			this.button5.Size = new System.Drawing.Size(81, 39);
+			this.button5.TabIndex = 4;
+			this.button5.Text = "Sair";
+			this.button5.UseVisualStyleBackColor = false;
+			this.button5.Click += new System.EventHandler(this.Button5Click);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(784, 362);
+			this.Controls.Add(this.button5);
+			this.Controls.Add(this.button4);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Name = "MainForm";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Caixas de Mensagem";
+			this.ResumeLayout(false);
+		}
+		private System.Windows.Forms.Button button5;
+		private System.Windows.Forms.Button button4;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button1;
+	}
+}
---
+++ MainForm.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace PjCxMessage
+{
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			InitializeComponent();
+			
+		}
+		
+		void Button1Click(object sender, EventArgs e)
+		{
+			MessageBox.Show("Bom dia");
+		}
+		
+		void Button2Click(object sender, EventArgs e)
+		{
+			MessageBox.Show("Olá galera, saudações", "Saudação");
+		}
+		
+		void Button3Click(object sender, EventArgs e)
+		{
+			if (MessageBox.Show("Deseja sair do programa?","Verificação",MessageBoxButtons.YesNo) == DialogResult.Yes)
+				Close();
+			else
+				MessageBox.Show("Oba, então o programa continuará em execução!");
+		}
+		
+		void Button4Click(object sender, EventArgs e)
+		{
+			MessageBox.Show("Quarto exemplo de MessageBox", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+		}
+		
+		void Button5Click(object sender, EventArgs e)
+		{
+			if (MessageBox.Show("Tem certeza que deseja sair do programa?", "Sair", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+			Close();
+		}
+	}
+}
